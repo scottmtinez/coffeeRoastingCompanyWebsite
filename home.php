2d7e369225da7212054df1e254e95ed04a149814
Scotty <scottmtinez@gmail.com>
2023-08-29 21:56:35 -0500

    Add files via upload

---
+++ home.php
@@ -0,0 +1,30 @@
+<?php SESSION_START(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="home.css"> 
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.4/font/bootstrap-icons.css">
+    <title>Coffee Roasting Company</title>
+</head>
+<body class="body-container">
+    <nav class="navbar-Container">
+        <a class="navbar-brand" href="#">Coffee Roasting Co.</a> <!-- NOTE: LOGO -->
+        <div class="navbar-links">
+            <a class="navbar-home" href="#">Home</a>
+            <a class="navbar-products" href="#">Products</a>
+            <a class="navbar-about" href="#">About</a>
+            <a class="navbar-contact" href="#">Contact</a>
+
+            <a class="navbar-search" href="#"><i class="bi bi-search"></i></a>
+            <a class="navbar-profile" href="#"><i class="bi bi-person-circle"></i></a>
+            <a class="navbar-cart" href="#"><i class="bi bi-bag"></i></a>
+        </div>
+    </nav>
+    
+    <div class="">
+
+    </div>
+</body>
+</html>
